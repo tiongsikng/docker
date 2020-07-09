7c37a1e8ed0f4aadb3c279e848447a02ba8e9571
TorukoFenix42 <49634577+TorukoFenix42@users.noreply.github.com>
2020-07-09 14:25:04 +0900

    Update ts_docker.sh

--- ts_docker.sh
+++ ts_docker.sh
@@ -5,10 +5,8 @@ sudo docker run -it \
 -p 8888:8888 \
 --network host \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v /home/zeke/dockerpython:/home/dockerpython \
--v /home/zeke/datasets:/home/datasets \
--v /media/videoDB:/media/videoDB \
--v /media/imageDB:/media/imageDB \
+-v /home/tiongsik/dockerpython:/home/dockerpython \
+-v /home/tiongsik/datasets:/home/datasets \
 --name tsng-tf \
 tensorflow/tensorflow /bin/bash\
 
