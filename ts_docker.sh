1bb22f54ec048d3fe9b4b38fab2541f3511fb467
Tiong-Sik Ng (황충식) <49634577+TorukoFenix42@users.noreply.github.com>
2020-07-16 15:41:18 +0900

    Update ts_docker.sh

--- ts_docker.sh
+++ ts_docker.sh
@@ -3,10 +3,10 @@ xhost local:docker
 
 sudo docker run -it \
 -p 8888:8888 \
+--gpus all \
 --network host \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v /home/tiongsik/dockerpython:/home/dockerpython \
 -v /home/tiongsik/datasets:/home/datasets \
---name tsng-tf \
-tensorflow/tensorflow /bin/bash\
-
+--name tsng-cuda \
+nvidia/cuda:10.0-base /bin/bash\
