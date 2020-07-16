6a882c4c8bc15c3a3c782fbcd86b8908536755d7
Tiong-Sik Ng (황충식) <49634577+TorukoFenix42@users.noreply.github.com>
2020-07-16 17:44:14 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,11 +2,8 @@
 apt-get update  # To get the latest package lists
 apt-get install python3 -y
 apt-get install python3-pip -y
-apt-get install python3-numpy -y
 apt-get install python3-dev -y
 apt install git -y
-apt-get install python3-matplotlib -y
-apt-get install python3-graphviz -y
 apt install python3-pil -y
 apt-get install libglib2.0-0 -y
 apt-get install libice6 libsm6 libxt6 libxrender1 libfontconfig1 libcups2 -y
